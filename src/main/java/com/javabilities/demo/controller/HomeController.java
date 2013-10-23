a8457a790365471ce82c08d3465290eee8dfe7a2
javabilities <meinholz@javabilities.com>
2013-10-23 01:49:04 -0400

    Adding javadoc.

--- src/main/java/com/javabilities/demo/controller/HomeController.java
+++ src/main/java/com/javabilities/demo/controller/HomeController.java
@@ -7,16 +7,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+/** This is the Controller for the home page
+ *
+ * @author Lloyd H. Meinholz
+ * @version 1.0
+ */
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+    /** The logger for HomeController */
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     /**
-     * loadHomePage
+     * This method will create a model containing a name for forward the request to the hom page.
      *
-     * @param m
-     * @return
+     * @param m    The model
+     * @return     The String representation of the page to forward the request to
      */
 //    @RequestMapping("home")
     public String loadHomePage(Model m) {
